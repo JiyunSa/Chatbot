14b3071b60201fc0227e2bf24c4d6efa88d56a6f
JiyunSa <81289580+JiyunSa@users.noreply.github.com>
2021-04-17 23:43:48 +0900

    Add files via upload

---
+++ SP-CW2/src/main/java/com/example/demo/SpCw2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpCw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpCw2Application.class, args);
+	}
+
+}
